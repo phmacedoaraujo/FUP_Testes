0852469483f272aa6cc5a8b1399955db9451b070
phmacedoaraujo <phmacedoaraujo@gmail.com>
2014-04-10 18:52:10 -0300

    Create aluno.cpp

---
+++ Teste01/aluno.cpp
@@ -0,0 +1,38 @@
+//2 pts
+/* Retornar o maior dos numeros A e B e C e D dados como entrada. */
+int valor_maximo4 (int a, int b, int c, int d)
+{
+
+}
+
+//1 pt
+/* Se o numero for negativo, inverter deixando positivo e retornar.
+* Se for positivo, retornar o valor positivo.
+*/
+int modulo (int num)
+{
+
+}
+
+//2 pt
+/* Se num1 e num2 forem ambos divisiveis por 3 ou forem ambos divisiveis
+* por 5 retorne true */
+int ambos_3_ou_ambos_5 (int num1, int num2)
+{
+if( (num1%3==0 && num2%3==0) || (num1%5==0 && num2%5==0) ) return true;
+else return false;
+}
+
+//2pt
+//Faça uma função que retorne quantos números ímpares e divisíveis por 3 existem entre 0 e 500.
+int impares_3_500()
+{
+    return 0;
+}
+
+//2pt
+//Programa que calcula a potencia inteira positiva de um numero
+float potencia(float base, int expoente)
+{
+    return 0;
+}
